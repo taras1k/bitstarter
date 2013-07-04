4d64c18f9d73efd2b6dc6f5bf13af013d2b9094a
Ubuntu <ubuntu@ip-172-31-4-14.us-west-2.compute.internal>
2013-07-04 11:49:41 +0000

    aded template

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  var template = fs.readFile('index.html');
+  response.send(template.toString());
 });
 
 var port = process.env.PORT || 5000;
