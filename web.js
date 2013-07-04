fa457c28668159b6372fd1af1b9386edffc37082
Taras Nek <ubuntu@ip-172-31-4-14>
2013-07-04 12:03:56 +0000

    fix file read

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var template = fs.readFile('index.html');
+  var template = fs.readFileSync('index.html');
   response.send(template.toString());
 });
 
